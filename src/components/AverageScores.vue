6a74d3ef98d6f06bbf0b6e82a1d9e42e9eef545f
Mustaqeem <mbangi66@gmail.com>
2024-01-30 08:12:50 +0300

    created AverageScores Component

---
+++ src/components/AverageScores.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+      <h1>Average Scores</h1>
+      <p>{{ averageScores }}</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        averageScores: null,
+      };
+    },
+    mounted() {
+      this.fetchAverageScores();
+    },
+    methods: {
+      async fetchAverageScores() {
+        try {
+          const response = await this.$axios.get('/api/average-scores');
+          this.averageScores = response.data.averageScores;
+        } catch (error) {
+          console.error('Error fetching average scores:', error);
+        }
+      },
+    },
+  };
+  </script>
