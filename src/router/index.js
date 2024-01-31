d842860a0c14869362a8ef13b2ba9a1c0bfc8278
Mustaqeem <mbangi66@gmail.com>
2024-01-31 11:12:50 +0300

    add path to AverageScores

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,15 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import AverageScores from '@/components/AverageScores.vue'
+import Home from "@/components/Home.vue"
+
 const routes = [
   {
     path: '/',
     name: 'Home',
+    component: Home,
+  },
+  {
+    path: '/AverageScores',
     component: AverageScores,
   },
 ]
