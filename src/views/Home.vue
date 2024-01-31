658cb511c952ce3449ffd7d51d7cf068344a9f87
Mustaqeem <mbangi66@gmail.com>
2024-01-31 15:41:01 +0300

    add Header and Footer

---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+   <!-- Header -->
+   <header class="bg-indigo-600 text-white p-5">
+      <div class="container mx-auto">
+        <h1 class="text-2xl font-semibold">Student Dashboard</h1>
+      </div>
+    </header>
+        <Dashboard />
+        <PerformanceByParentalEducation />
+      <!-- Footer -->
+        <footer class="bg-indigo-600 text-white p-4 mt-8">
+          <div class="container mx-auto">
+            <p>&copy; 2024 Your Website. All rights reserved.</p>
+          </div>
+        </footer>
+</template>
+  
+<script>
+  import Dashboard from '../components/Dashboard.vue';  
+  import PerformanceByParentalEducation from '../components/PerformanceByParentalEducation.vue';
+  
+  export default {
+    components: {
+      Dashboard,
+      PerformanceByParentalEducation
+    },
+    data() {
+      return {
+        error: null,
+      };
+    },
+  };
+</script>
+<style></style>
